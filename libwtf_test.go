d19e1745c38024acbf78793123f6eb6fb080b752
Will Sussman <wsussman@mit.edu>
2022-07-08 02:55:50 -0400

    gutted tests

--- libwtf_test.go
+++ libwtf_test.go
@@ -4,24 +4,24 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	a := 1
-	b := 2
-	expected := a + b
+// func TestAdd(t *testing.T) {
+// 	a := 1
+// 	b := 2
+// 	expected := a + b
 
-	if got := Add(a, b); got != expected {
-		t.Errorf("Add(%d, %d) = %d, didn't return %d", a, b, got, 
-expected)
-	}
-}
+// 	if got := Add(a, b); got != expected {
+// 		t.Errorf("Add(%d, %d) = %d, didn't return %d", a, b, got, 
+// expected)
+// 	}
+// }
 
-func TestSubtract(t *testing.T) {
-	a := 1
-	b := 2
-	expected := a - b
+// func TestSubtract(t *testing.T) {
+// 	a := 1
+// 	b := 2
+// 	expected := a - b
 
-	if got := Subtract(a, b); got != expected {
-		t.Errorf("Subtract(%d, %d) = %d, didn't return %d", a, b, 
-got, expected)
-	}
-}
+// 	if got := Subtract(a, b); got != expected {
+// 		t.Errorf("Subtract(%d, %d) = %d, didn't return %d", a, b, 
+// got, expected)
+// 	}
+// }
